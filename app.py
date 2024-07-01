1ec6f6b42c3905bec8a78607574bd7c571c28271
PavelMakarchuk <pavel.ma99@gmail.com>
2024-07-02 02:38:52 +0400

    Make header and tabs float Fixes #3

--- app.py
+++ app.py
@@ -2,10 +2,35 @@ import streamlit as st
 from policyengine_core.charts import *
 from household_impact import display_household_impact
 from societal_impact import display_societal_impact
+from streamlit_float import *
 
+# Initialize float feature/capability
+float_init()
 
-# Streamlit code for displaying the data
-st.title("UK 2024 Election Manifestos")
+# Custom CSS to ensure the floating header is positioned correctly
+st.markdown("""
+    <style>
+        .floating-header {
+            position: fixed;
+            top: 46px; 
+            width: 100%;
+            background-color: white;
+            z-index: 9999 !important;
+            padding: 10px 0;
+        }
+        .spacer {
+            height: 100px; /* Adjust this value to create enough space */
+        }
+    </style>
+    """, unsafe_allow_html=True)
+
+# Create a container for the floating header
+header_container = st.container()
+with header_container:
+    st.header("UK 2024 Election Manifestos")
+header_container.markdown('<div class="floating-header"></div>', unsafe_allow_html=True)
+header_container.float("top:50px; background-color: white; z-index: 9999;")
+
 st.markdown(
     "This interactive app compares the societal and household-level impacts of the Conservative, Labour and Liberal Democrat manifestos."
 )
